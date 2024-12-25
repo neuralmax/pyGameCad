ddeea3df5674b21cf32cada9f70ff4deb57ee866
neuralmax <neuralmax@github.com>
2024-12-25 14:14:01 +0200

    intermidiate

--- vaseModeGrid.py
+++ vaseModeGrid.py
@@ -21,89 +21,48 @@ class VaseModeGrid():
 			#cgi.addEntity('line',[[10,0],[20,10]])
 			#cgi.addEntity('circle',[[20,20],10])
 			#params
-			m=4
-			n=4
+
+			n=4#x
+			m=4#y
 			#solve
 			'''
 			data=[#example of hand solution
-				[[0,0],[1,0],[3,0],[3,0]],
-				[[0,3],[1,2],[1,2],[2,2]],
-				[[0,3],[1,3],[2,2],[2,3]],
-				[[0,1],[3,1],[3,1],[2,3]],
-			]
-			'''
-			'''
-			data=[#example of hand solution
-				[[[0,0],[0,0]],[[0,0],[1,0]],[[0,0],[1,1]],[[0,0],[1,1]],],
-				[[[1,1],[0,0]],[[0,1],[1,0]],[[0,1],[1,0]],[[0,1],[0,1]],],
-				[[[1,1],[0,0]],[[1,0],[1,0]],[[0,1],[1,1]],[[1,1],[0,1]],],
-				[[[1,0],[0,0]],[[1,0],[1,1]],[[1,0],[1,1]],[[1,1],[0,1]],]
-			]
-			'''
-			'''
-			nbrsa=[
-				[ 1, 0, 0],
-				[ 1, 1,-1],
-				[ 0, 0,-1],
-				[-1, 0, 0],
-				[ 0,-1,-1],
-				[ 1,-1,-1],
-			]
-			nbrsb=[
-				[ 0, 1, 1],
-				[ 0, 1, 0],
-				[-1, 1, 1],
-				[-1, 0, 1],
-				[ 0,-1, 0],
-				[ 0, 0, 1],
+				[[2,2,2,2],[2,2,1,2],[2,2,1,2],[2,2,1,2],[2,2,2,2]],
+				[[2,1,2,2],[0,0,1,1],[1,0,1,1],[1,0,0,1],[2,2,2,1]],
+				[[2,1,2,2],[0,1,1,1],[1,0,0,0],[1,1,0,1],[2,2,2,1]],
+				[[2,1,2,2],[0,1,1,0],[1,1,1,0],[1,1,0,0],[2,2,2,1]],
+				[[2,2,2,2],[1,2,2,2],[1,2,2,2],[1,2,2,2],[2,2,2,2]],
 			]'''
-			# neighbourhood connections
-			# 0 : z=1, d=1 <- horizontal leftwards
-			# 1 : z=0, d=1 |v vertical downwards
-			# 2 : z=1, d=2 -> horizontal rightwards
-			# 3 : z=0, d=2 |^ vertical upwards
-			# [ x<mod>, y<mod>, z(vertical/horzontal)<actual>, d(right,left,up,down)<actual> ]
-			'''
-			nbrs=[[#0
-				[ 0,-1,0,1],
-				[-1, 0,1,2],
-				[ 0, 0,0,0],
-			],[#1
-				[-1, 1,1,2],
-				[ 0, 1,0,2],
-				[ 0, 1,1,1],
-			],[#2
-				[ 1, 1,0,2],
-				[ 1, 1,1,1],
-				[-1, 1,0,1],
-			],[#3
-				[ 0, 0,1,1],
-				[ 0,-1,0,1],
-				[-1, 0,0,1],
-			]]
-			'''
-			#[z][d-1]=nc
-			dirs=[[0,2],[1,3]]
-			#init matrix
 			data=[[[[0,0],[0,0]] for y in range(n)] for x in range(m)]
-			front=[]
-			#seed matrix with init front
-			for i in range(1,n):
-				front.append([0  ,i,0,1])
-				front.append([m-1,i,0,2])
-			for i in range(1,m):
-				front.append([i,  0,1,1])
-				front.append([i,n-1,1,2])
+			front=[]#[x,y,z]
+			dirs=[[0,-1],[1,0],[0,1],[-1,0]]
+			opos=[2,3,0,1]
+			#seed matrix with dead ends init front
+			for x,y in [[0,0],[0,m-1],[n-1,0],[n-1,m-1]]:#corners
+				data[y][x]=[2,2,2,2]
+			for x in range(1,n-2):#top bottom
+				data[0][x]=[2,2,1,2]
+				data[n-1][x]=[1,2,2,2]
+				front.append([x,0,2])#[x,y,z]
+				front.append([x,n-1,0])
+			for y in range(1,m-2):#left right
+				data[y][0]=[2,1,2,2]
+				data[y][m-1]=[2,2,2,1]
+				front.append([0,y,1])#[x,y,z]
+				front.append([m-1,y,3])
 			#solve
 			counter=0#limiting for testing
 			self.countTo+=1
 			while len(front)>0 and counter<self.countTo:
 				counter+=1
-				x,y,z,d=front.pop(0)
+				x,y,z=front.pop(0)
+				data[y][x][z]=1
+				nx,ny=dirs[z]
+				nz=dirs
 				endpointFree=True
-				print('counter',counter,'dirs[z][d-1]',dirs[z][d-1])
+				#print('counter',counter,'dirs[z][d-1]',dirs[z][d-1])
 				for nx,ny,nz,nd in nbrs[dirs[z][d-1]]:
-					print('nx,ny,nz,nd',nx,ny,nz,nd)
+					#print('nx,ny,nz,nd',nx,ny,nz,nd)
 					if x+nx<=0 and x+nx<m-1 and y+ny<=0 and y+ny<n-1:
 						print('data[x+nx][y+ny][nz]',data[x+nx][y+ny][nz])
 						if data[x+nx][y+ny][nz]!=nd:
@@ -116,23 +75,39 @@ class VaseModeGrid():
 						if x+nx<0 and x+nx<m-1 and y+ny<0 and y+ny<n-1:
 							if data[x+nx][y+ny][nz]!=0:
 								front.append([[x+nx][y+ny][nz][nd]])
-			'''
+
 			#generate
 			cgi.createLayer('maze',(0,255,0))
-			for y,dat in enumerate(data):
-				for x,c in enumerate(dat):
-					if c[0][0]==1 and c[0][1]==0:
-						cgi.addEntity('line',[[x*10,y*10],[x*10+8,y*10]])
-					if c[0][0]==0 and c[0][1]==1:
-						cgi.addEntity('line',[[x*10+2,y*10],[x*10+10,y*10]])
-					if c[0][0]==1 and c[0][1]==1:
-						cgi.addEntity('line',[[x*10,y*10],[x*10+10,y*10]])
-					if c[1][0]==1 and c[1][1]==0:
-						cgi.addEntity('line',[[x*10,y*10],[x*10,y*10+8]])
-					if c[1][0]==0 and c[1][1]==1:
-						cgi.addEntity('line',[[x*10,y*10+2],[x*10,y*10+10]])
-					if c[1][0]==1 and c[1][1]==1:
-						cgi.addEntity('line',[[x*10,y*10],[x*10,y*10+10]])
+			for y in range(len(data)-1):
+				for x in range(len(data[y])-1):
+					for z in [1,2]:
+						d=data[y][x][z]
+						xn,yn=dirs[z]
+						dn=data[y+yn][x+xn][opos[z]]
+						#if x+xn>=0 and x+xn<m and y+yn>=0 and y+yn<n:
+						strx = 'y='+str(y)
+						strx+=' x='+str(x)
+						strx+=' z='+str(z)
+						strx+=' d='+str(d)
+						strx+=' nbrY='+str(y+yn)
+						strx+=' nbrX='+str(x+xn)
+						strx+=' nbrZ='+str(opos[z])
+						strx+=' nbrD='+str(dn)
+						print(strx)
+						if z==1:
+							if d==1 and dn==0:
+								cgi.addEntity('line',[[x*10,y*10],[x*10+8,y*10]])
+							elif d==1 and dn==1:
+								cgi.addEntity('line',[[x*10,y*10],[x*10+10,y*10]])
+							elif d==0 and dn==1:
+								cgi.addEntity('line',[[x*10+2,y*10],[x*10+10,y*10]])
+						if z==2:
+							if d==1 and dn==0:
+								cgi.addEntity('line',[[x*10,y*10],[x*10,y*10+8]])
+							elif d==1 and dn==1:
+								cgi.addEntity('line',[[x*10,y*10],[x*10,y*10+10]])
+							elif d==0 and dn==1:
+								cgi.addEntity('line',[[x*10,y*10+2],[x*10,y*10+10]])
 			cgi.createLayer('ends',(255,0,0))
 			for y in range(m+1):
 				for x in range(n+1):
